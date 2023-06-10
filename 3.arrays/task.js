39a030cfcf450d3e65b683b8b314888bd5651047
максим извеков <you@example.com>
2023-06-11 00:31:35 +0700

    Решение готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    return  arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, element, i, arr) => acc + element / arr.length, 0);
+    return result;
 }
